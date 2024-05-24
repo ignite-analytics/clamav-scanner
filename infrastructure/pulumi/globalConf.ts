37e104e3afd6691ddb3f7325f78d9b89acfb3444
Aleksandar Stojanov <me@fnd.works>
2024-05-24 13:56:21 +0200

    add dummy values to globalConf
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- infrastructure/pulumi/globalConf.ts
+++ infrastructure/pulumi/globalConf.ts
@@ -1,6 +1,6 @@
 export const name = 'clamav-scanner' // service name
-export const project = '' // GCP project ID
-export const region = '' // service location
+export const project = 'my-project' // GCP project ID
+export const region = 'europe-west1' // service location
 export const image = {
 	repository: 'ghcr.io/ignite-analytics/clamav-scanner',
 	tag: '0.1.0'
@@ -8,20 +8,20 @@ export const image = {
 export const schedule = {
 	cron: '37 */2 * * *',
 	timezone: 'Europe/Oslo',
-	location: '' // cheduler job location
+	location: 'europe-west1' // cheduler job location
 }
 export const service = {
 	port: 1337
 }
 export const event = {
 	type: 'google.cloud.storage.object.v1.finalized',
-	buckets: [] // [{name: 'bucket-name', location: 'EU'}]
+	buckets: [{name: 'bucket-name', location: 'EU'}]
 }
 export const storage = {
 	location: 'EU',
 	mirrorBucket: `${name}-mirror`,
 	quarantineBucket: `${name}-quarantine`,
-	logBucket: '' // where to store bucket logs
+	logBucket: 'my-log-bucket' // where to store bucket logs
 }
 export const kms = {
 	location: 'europe', // KMS keyring location
