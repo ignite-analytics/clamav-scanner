4a7683f5974c72fdd3ddd7052954a20d57ef50bb
Aleksandar Stojanov <me@fnd.works>
2024-05-24 15:22:29 +0200

    bump version
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- infrastructure/pulumi/globalConf.ts
+++ infrastructure/pulumi/globalConf.ts
@@ -3,7 +3,7 @@ export const project = 'my-project' // GCP project ID
 export const region = 'europe-west1' // service location
 export const image = {
 	repository: 'ghcr.io/ignite-analytics/clamav-scanner',
-	tag: '0.1.0'
+	tag: '0.2.0'
 }
 export const schedule = {
 	cron: '37 */2 * * *',
